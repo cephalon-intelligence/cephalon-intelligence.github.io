fb0ca79cc03b00e805b815c9d766b84c8b5b2ca7
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2022-12-11 02:42:33 -0500

    setup: create

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Cephalon Intelligence Organization GH Pages Site",
+    version="0.0.0",
+    author="Hart Traveller",
+    author_email="hart@cephalon.io",
+    packages=find_packages(),
+    include_package_data=True,
+)
